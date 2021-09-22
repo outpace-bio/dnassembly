ada8e17173c0e48e02ebbc4b08e98ae4b0ec404d
Joshua Scheck <49527681+jschec@users.noreply.github.com>
2021-09-22 13:33:15 -0700

    Indentation and unpacking list comprehension

--- dnassembly/utils/utils.py
+++ dnassembly/utils/utils.py
@@ -1,12 +1,25 @@
 #! /usr/bin/env python3
 from itertools import tee
+from typing import Dict, Tuple
 
-dna_basepairs = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C', 'a': 't', 't': 'a', 'g': 'c', 'c': 'g'}
+dna_basepairs = {
+    'A': 'T', 
+    'T': 'A', 
+    'C': 'G', 
+    'G': 'C', 
+    'a': 't', 
+    't': 'a', 
+    'g': 'c', 
+    'c': 'g'
+}
 
 # Thank you itertools cookbook!
 def pairwise(iterable):
     "s -> (s0,s1), (s1,s2), (s2, s3), ..."
+
+    # returns 2 independent iterators from a single iterable
     a, b = tee(iterable)
+    
     next(b, None)
     return zip(a, b)
 
@@ -26,36 +39,62 @@ def cycle_in_frames(iterable, frame=3):
             cycle_count += 1
 
 
-def reverse_complement(seq):
+def reverse_complement(sequence) -> str:
     "Get reverse complement of DNA sequence"
-    return ''.join(reversed([dna_basepairs[a] if a in dna_basepairs.keys() else a for a in seq]))
+
+    reversed_sequence = ""
+    
+    for nucleotide in sequence:
+        if nucleotide in dna_basepairs.keys():
+            reversed_sequence += reversed(dna_basepairs[nucleotide])
+        else:
+            reversed_sequence += reversed(nucleotide)
+
+    # TODO - check if above statement works
+    return ''.join(reversed([dna_basepairs[a] if a in dna_basepairs.keys() else a for a in sequence]))
 
 
 # --- Manually implemented b/c Biopython is a mess --- #
 
 # E. coli codon table: http://www.sci.sdsu.edu/~smaloy/MicrobialGenetics/topics/in-vitro-genetics/codon-usage.html
 # Listed in order of frequency
-res_to_codons = {'A': ['GCG', 'GCC', 'GCA', 'GCT'],
-                 'C': ['TGC', 'TGT'],
-                 'D': ['GAT', 'GAC'],
-                 'E': ['GAA', 'GAG'],
-                 'F': ['TTT', 'TTC'],
-                 'G': ['GGC', 'GGT', 'GGG', 'GGA'],
-                 'H': ['CAT', 'CAC'],
-                 'I': ['ATT', 'ATC', 'ATA'],
-                 'K': ['AAA', 'AAG'],
-                 'L': ['CTG', 'TTA', 'TTG', 'CTT', 'CTC', 'CTA'],
-                 'M': ['ATG'],
-                 'N': ['AAC', 'AAT'],
-                 'P': ['CCG', 'CCA', 'CCT', 'CCC'],
-                 'Q': ['CAG', 'CAA'],
-                 'R': ['CGT', 'CGC', 'CGG', 'CGA', 'AGA', 'AGG'],
-                 'S': ['AGC', 'TCT', 'TCC', 'TCG', 'AGT', 'TCA'],
-                 'T': ['ACC', 'ACA', 'ACG', 'ACT'],
-                 'V': ['GTG', 'GTT', 'GTC', 'GTA'],
-                 'W': ['TGG'],
-                 'Y': ['TAT', 'TAC'],
-                 '*': ['TAA', 'TGA', 'TAG'],
-                 }
+res_to_codons = {
+    'A': ['GCG', 'GCC', 'GCA', 'GCT'],
+    'C': ['TGC', 'TGT'],
+    'D': ['GAT', 'GAC'],
+    'E': ['GAA', 'GAG'],
+    'F': ['TTT', 'TTC'],
+    'G': ['GGC', 'GGT', 'GGG', 'GGA'],
+    'H': ['CAT', 'CAC'],
+    'I': ['ATT', 'ATC', 'ATA'],
+    'K': ['AAA', 'AAG'],
+    'L': ['CTG', 'TTA', 'TTG', 'CTT', 'CTC', 'CTA'],
+    'M': ['ATG'],
+    'N': ['AAC', 'AAT'],
+    'P': ['CCG', 'CCA', 'CCT', 'CCC'],
+    'Q': ['CAG', 'CAA'],
+    'R': ['CGT', 'CGC', 'CGG', 'CGA', 'AGA', 'AGG'],
+    'S': ['AGC', 'TCT', 'TCC', 'TCG', 'AGT', 'TCA'],
+    'T': ['ACC', 'ACA', 'ACG', 'ACT'],
+    'V': ['GTG', 'GTT', 'GTC', 'GTA'],
+    'W': ['TGG'],
+    'Y': ['TAT', 'TAC'],
+    '*': ['TAA', 'TGA', 'TAG'],
+}
 
+
+def build_codon_to_res() -> Dict[str, str]:
+    codon_to_res = {}
+
+    for res, codons in res_to_codons.items():
+        for codon in codons:
+            codon_to_res[codon] = res
+    
+    return codon_to_res
+
+# TODO - check if above statement works
 codon_to_res = {codon: res for res, codons in res_to_codons.items() for codon in codons}
+
+
+
+
